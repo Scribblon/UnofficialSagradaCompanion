43ca9445daec1714f63ce1937269bf9fdaca4560
Coen Meulenkamp <coenmeulenkamp@live.nl>
2019-10-07 20:50:50 +0200

    Clean current implementation of the board select buttons.

--- UnofficialSagradaCompanion/UnofficialSagradaCompanion/ViewModels/BoardSelectViewModel.cs
+++ UnofficialSagradaCompanion/UnofficialSagradaCompanion/ViewModels/BoardSelectViewModel.cs
@@ -10,21 +10,11 @@ namespace UnofficialSagradaCompanion.ViewModels
 {
     public class BoardSelectViewModel : ViewModelBase
     {
-        public ICommand PressBoardButton { get; private set; }
-
-        private Dictionary<PlayerBoard, bool> PlayerSelect;
+         Dictionary<PlayerBoard, bool> PlayerSelect;
 
         public BoardSelectViewModel()
         {
-            PlayerSelect = DefaultSettingsModel.DefaultActiveBoards;
 
-            PressBoardButton = new Command((PlayerBoard) =>
-            {
-                //It works. Figure out how to interpret the input and change the stuff.
-                Console.WriteLine("Button Pressed! ");
-            }
-            );
-            
         }
     }
 }
